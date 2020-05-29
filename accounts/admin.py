fac59674b9cf88a0d615ef4d19e9e1f73bb5d409
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-28 22:20:35 -0700

    render data to dashboard

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,11 +5,8 @@ from .models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'customer', 'date_created']
 admin.site.register(Order, OrderAdmin)
-
-
-
-admin.site.register(Tag)
