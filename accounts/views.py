d2d1cb21403d44a77acae0e5d3aab97c603403c1
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-29 22:05:36 -0700

    dynamic customer urls

--- accounts/views.py
+++ accounts/views.py
@@ -30,5 +30,13 @@ def products(request):
     context = {'products': products}
     return render(request, 'accounts/products.html', context)
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context = {
+        'customer': customer,
+        'orders': orders,
+        'order_count': order_count,
+    }
+    return render(request, 'accounts/customer.html', context)
