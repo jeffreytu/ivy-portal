5f5b407ce34dd30c7e973bd828f08197eb25d279
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-30 10:44:53 -0700

    hook update and delete functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -40,3 +41,41 @@ def customer(request, pk_test):
         'order_count': order_count,
     }
     return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {
+        'form': form
+        }
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+        'form': form,
+        'order': order,
+        }
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+    context = {
+        'order': order
+    }
+    return render(request, 'accounts/delete.html', context)
