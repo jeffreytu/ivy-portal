94e307c7a6b54a15f904402b26e5cb1529789cd6
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-31 11:50:41 -0700

    connect user to customer relation

--- accounts/views.py
+++ accounts/views.py
@@ -23,6 +23,9 @@ def registerPage(request):
             username = form.cleaned_data.get('username')
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
             messages.success(request, 'Account was created for ' + username )
             return redirect('login')
 
@@ -73,8 +76,21 @@ def home(request):
     }
     return render(request,'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    return render(request, 'accounts/user.html')
+    orders = request.user.customer.order_set.all()
+
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending,    
+    }
+    return render(request, 'accounts/user.html', context)
 
 @login_required(login_url='login')
 def products(request):
