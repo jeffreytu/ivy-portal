76c13c2fbe31fbef0ed8653b1db6e45ab65ef0f3
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-31 16:21:33 -0700

    created signal for customer creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Customer(models.Model):
@@ -19,7 +20,6 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
 
     CATEGORY = (
@@ -36,7 +36,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-
 
 class Order(models.Model):
 
