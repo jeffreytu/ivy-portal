0e17ce12a238b045cc3cc3d2ff5955ff30b5cd71
Jeffrey <6518347+jeffreytu@users.noreply.github.com>
2020-05-31 17:04:34 -0700

    add password reset and replace templates

--- src/settings.py
+++ src/settings.py
@@ -113,3 +113,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'youremail'
+EMAIL_HOST_PASSWORD = 'your email pass'
